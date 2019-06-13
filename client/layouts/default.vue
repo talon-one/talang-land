05cfa1edb6fc07cc56cc32bbc34340e40062206e
Jakob (Koby) Shimony <shimony@talon.one>
2019-06-14 00:13:13 +0200

    Add link to Talang repo in the toolbar

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,12 +1,21 @@
 <template>
   <v-app dark>
     <v-toolbar class="fix-high" fixed app>
-      <nuxt-link to="/">
+      <nuxt-link to="/" title="Home">
         <img src="/favicon.ico" alt="Talang" width="36px" height="36px" />
       </nuxt-link>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <nuxt-link to="/about">About</nuxt-link>
+      <v-btn
+        href="https://github.com/talon-one/talang/"
+        title="https://github.com/talon-one/talang/"
+        target="_blank"
+        flat
+      >
+        GitHub
+        <v-icon right>open_in_new</v-icon>
+      </v-btn>
+      <v-btn to="/about" title="About" nuxt flat>About</v-btn>
     </v-toolbar>
     <v-content>
       <v-container>
